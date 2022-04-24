20e79dae4641119dfc358001224fda60199d9528
이준형 <lu1z5ec@naver.com>
2022-04-24 18:39:13 +0900

    source1.py
    간단 계산기 작성

---
+++ source1.py
@@ -0,0 +1,24 @@
+#계산기
+#a,b 변수 상자에 각각 정수형 데이터 저장
+a=100
+b=20
+
+#더하기
+result=a+b
+print(a, '+' ,b, '=' ,result)
+
+#뺄셈
+result = a-b
+print(a,'-',b,'=',result)
+
+#곱셈
+result=a*b
+print(a,'+',b,'=',result)
+
+#몫
+result=a/b
+print(a,'/',b,'=',result)
+
+#나머지
+result = a%b
+print(a,'/',b,'=',result)
