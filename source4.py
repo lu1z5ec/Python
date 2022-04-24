7b7da874caecd7a7db1f44c5fd1893a34c0f48af
이준형 <lu1z5ec@naver.com>
2022-04-24 19:27:38 +0900

    거북이 turtle 작성1
    거북이 turtle

---
+++ source4.py
@@ -0,0 +1,25 @@
+# 터틀 프로그램 기본 틀 만들기
+#라이브러리 호출
+import turtle
+
+#모양 (네모)
+turtle.shape('turtle')
+
+#오른쪽 90도 회전 / forward : 앞으로 200 이동
+turtle.right(90)
+turtle.forward(200)
+
+turtle.left(90)
+turtle.forward(200)
+
+turtle.left(90)
+turtle.forward(200)
+
+turtle.left(90)
+turtle.forward(200)
+
+# back : 뒤로 150 이동
+turtle.back(150)
+
+# done() 화면에 출력
+turtle.done()
